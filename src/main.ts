a588f2f94f59e03cee325635cee3d52aa933fa9c
DmytroQIvanov <20IvanovDmitriy03@gmail.com>
2021-07-19 15:55:47 +0300

    make it better

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  // app.enableCors({ origin: '*' });
+
+  await app.listen(process.env.PORT || 8080);
+  console.log('Server has been started!');
+
 }
 bootstrap();
